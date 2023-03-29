2825d53214044c498ddb650be0767a36825a9474
Ian Lyttle <ian.lyttle@se.com>
2023-03-29 17:25:17 -0500

    try script

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# clean bin directory
 if [[ -d bin ]]; then
     rm -r bin
 fi
@@ -7,6 +8,11 @@ mkdir bin
 
 cp src/quig.sh bin/quig
 
+filename="quig.tar.gz"
+
 cd ..
-tar --exclude=".git*" --exclude="src" -czvf quig.tar.gz quig
+tar --exclude=".git*" --exclude="src" -czvf $filename quig
 cd quig
+
+echo $filename
+exit 0;
