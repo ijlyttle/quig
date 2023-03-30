d59edba7cead474be992871d0fdc0898147db8ec
Ian Lyttle <ian.lyttle@se.com>
2023-03-30 14:45:15 -0500

    redirect to standard error

--- src/build-tarball.sh
+++ src/build-tarball.sh
@@ -11,7 +11,7 @@ cp src/quig.sh bin/quig
 filename="quig.tar.gz"
 
 cd ..
-tar --exclude=".git*" --exclude="src" -czvf $filename quig
+tar --exclude=".git*" --exclude="src" -czvf $filename quig >&2
 filepath=$(realpath $filename)
 cd quig
 
